f896b1757380151f372ee81104b64ca1c5234bbe
Weiwei <tyler22501789@gmail.com>
2020-12-17 10:23:20 +0800

    Initial Commit

---
+++ EmptyVIew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmptyVIew
+//
+//  Created by 羅壽之 on 2020/7/9.
+//  Copyright © 2020 羅壽之. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
